ad333e96ab8da1a4b53479b02c3462884e9b8c83
Kate Cobey <katherine_cobey@gmail.com>
2022-10-01 13:15:59 -0400

    Go to the 'whalefall' scene to check out food falling from the sky, food object. Would be great if we could pull this and have everyone use the same food object...

---
+++ Kebash/Assets/Scripts/Items/FoodFall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodFall : MonoBehaviour
+{
+    public float fallSpeed = 0.5f; //how fast it falls
+    public float timeout = 10f; //how long before it disappears
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        StartCoroutine(Timeout());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 1) {
+            transform.Translate(0, -fallSpeed, 0);
+        }
+    }
+    IEnumerator Timeout(){
+        yield return new WaitForSeconds(timeout);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Kebash/Assets/Scripts/Items/FoodSpawn.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FoodSpawn : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    // for easy editing of variables
+    //upper and lower bound for how long before food will appear
+    private float minTimeSpawn = 3f;
+    private float maxTimeSpawn = 10f;
+    //if we can attach this to the size of the field we can make it automatic, for now, we manually change this
+    //basically, boundaries for where the food will spawn. 
+    private float zSpawn = 17.5f;
+    private float xSpawn = 27.5f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnFood());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnFood()
+    {
+        while(true)
+        {
+            float timeRange = Random.Range(3f, 10f);
+            float x = (xSpawn * 0.5f) - 0.5f; //not spawning RIGHT on the edge and accounting for field originating at 0,0
+            float xRange = Random.Range(-x, x);
+            float z = (zSpawn * 0.5f) - 0.5f;
+            float zRange = Random.Range(-z, z);
+
+            yield return new WaitForSeconds(timeRange);
+            Instantiate(foodPrefab, new Vector3(xRange, 15, zRange), Quaternion.identity);
+        }
+    }
+}
+
+
+
