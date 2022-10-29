a6d0afb8721149c011eccc1fbe2a8a9ff010fe31
Jack Cobey <katherine_cobey@brown.edu>
2022-10-29 14:14:35 -0400

    bugs obliterated by jack cobey
    jack cobey bug obliterator. (bugs with nullreference when hitting food w bullet)

--- Kebash/Assets/Scripts/Items/FoodCollision.cs
+++ Kebash/Assets/Scripts/Items/FoodCollision.cs
@@ -7,31 +7,27 @@ using UnityEngine.InputSystem;
 public class FoodCollision : MonoBehaviour
 {
   private Rigidbody _rigidbody;
-    
-    // ================== Methods
 
-    void Start()
+  // ================== Methods
+
+  void Start()
   {
     _rigidbody = GetComponent<Rigidbody>();
-    }
+  }
 
   void OnTriggerEnter(Collider other)
-  {
-        // Collided with incoming damager
-        if (other.gameObject.layer == Utils.DamagerLayer) {
-            // TODO: damager can be charging player, or food projectile. Must disambiguate.
-            if (other.transform.parent.gameObject.GetComponent<Movement>().IsCharging)
+  { 
+    // Collided with incoming damager
+    if (other.gameObject.CompareTag("Player")) {
+      // TODO: damager can be charging player, or food projectile. Must disambiguate.
+      if (other.transform.parent.gameObject.GetComponent<Movement>().IsCharging)
       {
-
         bool foodWasTaken = 
           other.transform.parent.gameObject.GetComponent<Movement>()
           .AddFood(this.gameObject.GetComponent<FoodData>().Num);
-                other.transform.parent.gameObject.GetComponent<Movement>().PlayStab();
-
-                if (foodWasTaken) {
-
-                    this.gameObject.SetActive(false);
-                }         
+          other.transform.parent.gameObject.GetComponent<Movement>().PlayStab();
+          
+        if (foodWasTaken) this.gameObject.SetActive(false);
       }
     }
   }
