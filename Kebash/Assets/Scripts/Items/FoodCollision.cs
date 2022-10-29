e9ac5cea84c655deb2f0463711bbe54578d2f8af
Jack Cobey <katherine_cobey@brown.edu>
2022-10-29 15:31:38 -0400

    food haver
    food haver

--- Kebash/Assets/Scripts/Items/FoodCollision.cs
+++ Kebash/Assets/Scripts/Items/FoodCollision.cs
@@ -20,15 +20,11 @@ public class FoodCollision : MonoBehaviour
     // Collided with incoming damager
     if (other.gameObject.CompareTag("Player")) {
       // TODO: damager can be charging player, or food projectile. Must disambiguate.
-      if (other.transform.parent.gameObject.GetComponent<Movement>().IsCharging)
-      {
         bool foodWasTaken = 
           other.transform.parent.gameObject.GetComponent<Movement>()
           .AddFood(this.gameObject.GetComponent<FoodData>().Num);
           other.transform.parent.gameObject.GetComponent<Movement>().PlayStab();
-          
         if (foodWasTaken) this.gameObject.SetActive(false);
-      }
     }
   }
 
