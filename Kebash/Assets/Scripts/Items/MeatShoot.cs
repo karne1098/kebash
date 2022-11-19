cfdb04641764a7b7d64e103fd0b997a318aa9149
Jack Cobey <katherine_cobey@brown.edu>
2022-11-19 16:06:36 -0500

    meat n mushrooms
    still glitchy!

--- Kebash/Assets/Scripts/Items/MeatShoot.cs
+++ Kebash/Assets/Scripts/Items/MeatShoot.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class MeatShoot : MonoBehaviour
 {
+    public int large = 0;
+    public GameObject flesh;
+    public Rigidbody fleshBody;
 
     void FixedUpdate()
     {
-        Vector3 _currentForce = transform.GetComponent<Rigidbody>().velocity;
-        transform.GetComponent<Rigidbody>().velocity = new Vector3(_currentForce.x * .8f, _currentForce.y, _currentForce.z * .8f);
+        Vector3 _currentForce = fleshBody.velocity;
+        fleshBody.velocity = new Vector3(_currentForce.x * .8f, _currentForce.y, _currentForce.z * .8f);
+        if(large < 6){
+            float a = flesh.transform.localScale.x;
+            flesh.transform.localScale = new Vector3(1.4f * a, flesh.transform.localScale.y, flesh.transform.localScale.z);
+            large += 1;
+        }
         
     }
 }
--- Kebash/Assets/Scripts/Items/MushroomShoot.cs
+++ Kebash/Assets/Scripts/Items/MushroomShoot.cs
@@ -13,14 +13,17 @@ public class MushroomShoot : MonoBehaviour
     {
         GameObject bullet = Instantiate<GameObject>(bulletPrefab);
         bullet.transform.position = transform.position;
- 
-        mushroomBody.AddForce(Quaternion.AngleAxis(angleOffset, Vector3.forward) * transform.right * 100.0f);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 20f;
+        bullet.GetComponent<FoodShootBase>().StartShot(bullet.transform);
+        bullet.GetComponent<Rigidbody>().AddForce(Quaternion.AngleAxis(angleOffset, Vector3.forward) * transform.right * 100.0f);
    }
 
     // Start is called before the first frame update
     void Start()
     {
         CreateBullet(bulletPrefab, -30f);
+        CreateBullet(bulletPrefab, -15f);
+        CreateBullet(bulletPrefab, 15f);
         CreateBullet(bulletPrefab, 30f);
     }
  
